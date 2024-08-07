6e804a1d9552a7d9201cc2462b4cb4f977314cea
RaghvendraShukla100 <158793912+RaghvendraShukla100@users.noreply.github.com>
2024-08-07 17:45:11 +0000

    read and write command is added

--- FS-Module/index.js
+++ FS-Module/index.js
@@ -1,1 +1,14 @@
-console.log('Hello developers')
+const fs=require('fs')
+
+// async function
+fs.writeFile('./test.txt', 'my name is Raghvendra shukla \nI love codeing.',(err)=>{
+    // console.log(err)
+})
+
+fs.readFile('./test.txt','utf-8',(err,result)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log(result)
+    }
+})
